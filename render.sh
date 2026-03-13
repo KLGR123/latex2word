13d9e38f1d89117d541b8b8e496266fe12423c97
Jiarun Liu <liujiarun01@126.com>
2026-03-13 10:30:05 +0800

    remove pythonic rendering

--- render.sh
+++ render.sh
@@ -7,7 +7,3 @@ OUTPUTS_DIR=/Users/liujiarun/Downloads/latex2word/outputs
 
 python3 label.py --input ${OUTPUTS_DIR}/translated.json --output ${OUTPUTS_DIR}/labeled.json
 python3 refmap.py --input ${OUTPUTS_DIR}/labeled.json --output ${OUTPUTS_DIR}/refmap.json --verbose
-
-python3 render.py --labeled ${OUTPUTS_DIR}/labeled.json \
-  --refmap ${OUTPUTS_DIR}/refmap.json --citations ${OUTPUTS_DIR}/citations.json --inputs-dir ${INPUTS_DIR}/ \
-  --output ${OUTPUTS_DIR}/result.docx --toc --font-size 12 --page-size a4
