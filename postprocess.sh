cd8a036dff12ac278a6d80625732ae9418ecd040
Jiarun Liu <liujiarun01@126.com>
2026-03-15 17:05:52 +0800

    Update postprocess.sh

--- postprocess.sh
+++ postprocess.sh
@@ -5,16 +5,16 @@ shopt -s nullglob
 INPUTS_DIR=/Users/liujiarun/Downloads/latex2word/inputs
 OUTPUTS_DIR=/Users/liujiarun/Downloads/latex2word/outputs
 
-# python3 label.py --input ${OUTPUTS_DIR}/translated.json --output ${OUTPUTS_DIR}/labeled.json
-# python3 refmap.py --input ${OUTPUTS_DIR}/labeled.json --output ${OUTPUTS_DIR}/refmap.json --verbose
-# rm ${OUTPUTS_DIR}/translated.json
+python3 label.py --input ${OUTPUTS_DIR}/translated.json --output ${OUTPUTS_DIR}/labeled.json
+python3 refmap.py --input ${OUTPUTS_DIR}/labeled.json --output ${OUTPUTS_DIR}/refmap.json --verbose
+rm ${OUTPUTS_DIR}/translated.json
 
-# python3 replace.py \
-#   --labeled   ${OUTPUTS_DIR}/labeled.json \
-#   --citations ${OUTPUTS_DIR}/citations.json \
-#   --refmap    ${OUTPUTS_DIR}/refmap.json \
-#   --output    ${OUTPUTS_DIR}/replaced.json \
-#   --verbose
-# rm ${OUTPUTS_DIR}/labeled.json
+python3 replace.py \
+  --labeled   ${OUTPUTS_DIR}/labeled.json \
+  --citations ${OUTPUTS_DIR}/citations.json \
+  --refmap    ${OUTPUTS_DIR}/refmap.json \
+  --output    ${OUTPUTS_DIR}/replaced.json \
+  --verbose
+rm ${OUTPUTS_DIR}/labeled.json
 
 python3 render.py --json ${OUTPUTS_DIR}/replaced.json --docx ${OUTPUTS_DIR}/final.docx --citations ${OUTPUTS_DIR}/citations.json
